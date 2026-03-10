f42ceb8dcdcd060964bb71f216163986dd18b4fb
kostas900402 <nascoskostas@gmail.com>
2026-03-10 07:59:04 +0100

    SB

---
+++ src/main/java/com/example/boilerroom_labb1/BoilerroomLabb1Application.java
@@ -0,0 +1,13 @@
+package com.example.boilerroom_labb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerroomLabb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerroomLabb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boilerroom_labb1/BoilerroomLabb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boilerroom_labb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerroomLabb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
